de997feca56fa56b7ff0746458752ece6a9edd8c
Mudit Rajput <mudit.rajput@successive.tech>
2021-01-05 18:49:35 +0530

    feat: merge branch with develop (#40950)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
-import { NavBar, TraineeComponent } from './pages';
+import { CssBaseline } from '@material-ui/core';
+import { NavBar, TraineeComponent, LoginUi } from './pages';
 import theme from './theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <NavBar />
       <TraineeComponent />
+      <LoginUi />
     </ThemeProvider>
   );
 }
